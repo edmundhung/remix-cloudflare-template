07589e798cc02649021b77b3a244e458121c24c0
Edmund Hung <me@edmund.dev>
2021-08-23 00:02:12 +0200

    feat: bootstrap with the remix-app-template

---
+++ app/root.tsx
@@ -0,0 +1,57 @@
+import type { LinksFunction, LoaderFunction } from "remix";
+import { Meta, Links, Scripts, useRouteData, LiveReload } from "remix";
+import { Outlet } from "react-router-dom";
+
+import stylesUrl from "./styles/global.css";
+
+export let links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export let loader: LoaderFunction = async () => {
+  return { date: new Date() };
+};
+
+function Document({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  let data = useRouteData();
+  return (
+    <Document>
+      <Outlet />
+      <footer>
+        <p>This page was rendered at {data.date.toLocaleString()}</p>
+      </footer>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document>
+      <h1>App Error</h1>
+      <pre>{error.message}</pre>
+      <p>
+        Replace this UI with what you want users to see when your app throws
+        uncaught errors.
+      </p>
+    </Document>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,34 @@
+import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
+import { useRouteData } from "remix";
+
+import stylesUrl from "../styles/index.css";
+
+export let meta: MetaFunction = () => {
+  return {
+    title: "Remix Starter",
+    description: "Welcome to remix!"
+  };
+};
+
+export let links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export let loader: LoaderFunction = async () => {
+  return { message: "this is awesome 😎" };
+};
+
+export default function Index() {
+  let data = useRouteData();
+
+  return (
+    <div style={{ textAlign: "center", padding: 20 }}>
+      <h2>Welcome to Remix!</h2>
+      <p>
+        <a href="https://remix.run/dashboard/docs">Check out the docs</a> to get
+        started.
+      </p>
+      <p>Message from the loader: {data.message}</p>
+    </div>
+  );
+}
