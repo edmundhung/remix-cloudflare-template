8ff8a9c64e56d3d7bb1b1d4b7125d3d0812cac43
Edmund Hung <me@edmund.dev>
2021-11-14 23:47:39 +0100

    chore: improve build script with build result info

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,16 +1,30 @@
 import * as esbuild from 'esbuild';
 
-const mode = process.env.NODE_ENV?.toLowerCase() ?? 'development';
+async function build() {
+  const mode = process.env.NODE_ENV?.toLowerCase() ?? 'development';
 
-console.log(`[Worker] Running esbuild in ${mode} mode`);
+  console.log(`Building Worker in ${mode} mode`);
 
-esbuild.build({
-  entryPoints: ['./worker/index.ts'],
-  bundle: true,
-  minify: mode === 'production',
-  format: 'esm',
-  define: {
-    "process.env.NODE_ENV": `"${mode}"`,
-  },
-  outfile: 'worker.js',
-});
+  const outfile = 'worker.js';
+  const startTime = Date.now();
+  const result = await esbuild.build({
+    entryPoints: ['./worker/index.ts'],
+    bundle: true,
+    minify: mode === 'production',
+    format: 'esm',
+    metafile: true,
+    define: {
+      "process.env.NODE_ENV": `"${mode}"`,
+    },
+    outfile,
+  });
+  const endTime = Date.now();
+
+  console.log(`Built in ${endTime - startTime}ms`);
+
+  if (mode === 'production') {
+    console.log(await esbuild.analyzeMetafile(result.metafile));
+  }
+}
+
+build().catch(e => console.error('Unknown error caught during build:', e));
