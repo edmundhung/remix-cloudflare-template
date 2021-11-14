191c9458f1e63aab9ccf4567275296ea7d3e7163
Edmund Hung <me@edmund.dev>
2021-11-14 23:44:05 +0100

    cfeat: share tsconfig between remix and worker

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
- module.exports = {
+module.exports = {
   appDirectory: "app",
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
---
+++ remix.worker.d.ts
@@ -0,0 +1,5 @@
+export {};
+
+declare global {
+  // const BINDING_NAME: KVNamespace;
+}
