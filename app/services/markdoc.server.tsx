ff35cd2b06c06b99b34e37b0280e0e14e3ee12a4
Edmund Hung <edmund@cloudflare.com>
2026-05-05 23:19:30 +0100

    cleanup

--- app/services/markdoc.server.tsx
+++
@@ -1,8 +0,0 @@
-import markdoc from '@markdoc/markdoc';
-
-export function parse(markdown: string) {
-	const ast = markdoc.parse(markdown);
-	const node = markdoc.transform(ast);
-
-	return node;
-}
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-import typeography from '@tailwindcss/typography';
-
-export default {
-	content: ['./app/**/*.tsx', './app/**/*.ts'],
-	theme: {},
-	plugins: [typeography],
-};
